6d78337ed9a42a5992e24a7f128c04893fdf8308
Ashima18Muyal <132137662+Ashima18Muyal@users.noreply.github.com>
2024-03-10 13:36:43 +0530

    Delete dsa-graded-assignment 2 directory

--- dsa-graded-assignment 2/src/gradedassignmentDSA/DriverApp.java
+++
@@ -1,9 +0,0 @@
-package gradedassignmentDSA;
-
-public class DriverApp {
-	public static void main(String[] args) {
-		Numberofdays mt = new Numberofdays();
-		mt.accept();
-		mt.calculate();
-	}
-}
